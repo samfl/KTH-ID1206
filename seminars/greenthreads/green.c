0e52ac9fb290f7ac9bb0e6904dae667a5e6cbdab
Sam Florin <sam.florin@hotmail.com>
2020-12-26 12:05:06 +0100

    update

--- seminars/greenthreads/green.c
+++ seminars/greenthreads/green.c
@@ -10,6 +10,8 @@
 
 static ucontext_t main_cntx = {0};
 static green_t main_green = {&main_cntx, NULL, FALSE}
+static green_t* running = &main_green;
+
 static void init() __attribute__((constructor));
 
 /* Initializes the main_cntx so when we call the scheduling function for the first time the running thread will be properly initialized. */
@@ -18,6 +20,25 @@ void init()
     getcontext(&main_cntx);
 }
 
+void green_thread(void)
+{
+    green_t* this = running;  
+
+    void* result = (*this->fun)(this->arg);
+
+    // Place waiting (joining) thread in ready queue. 
+
+    // Save result of execution.
+
+    // We're a zombie.
+
+    // Find the next thread to run.
+
+    running = next; 
+    setcontext(next->context);
+}
+
+/* Initializes a green thread. */
 int green_create(green_t* new, void* (*fun) (void*), void* arg)
 {
     ucontext_t* cntx = (ucontext_t*) malloc(sizeof(ucontext_t));
@@ -42,24 +63,7 @@ int green_create(green_t* new, void* (*fun) (void*), void* arg)
     return 0; 
 }
 
-void green_thread(void)
-{
-    green_t* this = running;  
-
-    void* result = (*this->fun)(this->arg);
-
-    // Place waiting (joining) thread in ready queue. 
-
-    // Save result of execution.
-
-    // We're a zombie.
-
-    // Find the next thread to run.
-
-    running = next; 
-    setcontext(next->context);
-}
-
+/* Suspends the current thread and selects a new thread for execution. */
 int green_yield(void)
 {
     green_t* susp = running; 
@@ -74,6 +78,7 @@ int green_yield(void)
     return 0; 
 }
 
+/* The current thread is suspended waiting for a thread to terminate. */
 int green_join(green_t* thread, void** res)
 {
 
