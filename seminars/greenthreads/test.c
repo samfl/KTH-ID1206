e48edf21b9396eefc2e9a58f4e154c5e4e4cf264
Sam Florin <sam.florin@hotmail.com>
2020-12-29 17:26:15 +0100

    Added tests for interrrupt timer, template for prod/cons

--- seminars/greenthreads/test.c
+++ seminars/greenthreads/test.c
@@ -1,12 +1,15 @@
 #include "green.h"
 
+#define LOOP 1000000
+
 int flag = 0; 
+int count = 0; 
 green_cond_t cond; 
 
-void* test_thread(void* arg)
+void* test_yield(void* arg)
 {
     int i = *(int*) arg; 
-    int loop = 4; 
+    int loop = LOOP; 
     while (loop > 0) 
     {
         printf("thread %d: %d\n",  i, loop);
@@ -19,7 +22,7 @@ void* test_thread(void* arg)
 void* test_condv(void* arg)
 {
     int id = *(int*) arg; 
-    int loop = 4;
+    int loop = LOOP; 
 
     while (loop > 0)
     {
@@ -37,6 +40,116 @@ void* test_condv(void* arg)
     return (void*) 0;
 }
 
+void* test_interrupts(void* arg)
+{
+    int id = *(int*) arg; 
+    int loop = LOOP; 
+
+    while (loop > 0)
+    {
+        if (flag == id)
+        {
+            printf("thread %d: %d\n", id, loop);
+            loop--;
+            flag = (id + 1) % 2;
+            green_cond_signal(&cond);
+        } else
+        {
+            green_cond_wait(&cond); 
+        }
+    }
+    return (void*) 0;
+}
+
+/* Shared resource, global shared counter */
+void* test_interrupts_fail(void* arg)
+{
+    int id = *(int*) arg; 
+    for (int i = 0; i < 1000000; i++)
+    {   
+        /* LOCK NEEDED */
+        int tmp = count; 
+
+        /* Create some delay between Read / Write */
+        int dummy = 0; 
+        if (tmp > 1000)
+        {
+            ++dummy;
+        }
+        tmp++; 
+        /* ****************************************/
+
+        count = tmp; 
+        /* UNLOCK NEEDED */
+    }
+    return (void*) 0;
+}
+
+#define MAX 10
+int buffer[MAX];
+int fill_ptr = 0; 
+int use_ptr = 0;
+int loops = LOOP;
+green_cond_t empty;
+green_cond_t fill; 
+
+/* Helpers for Producer / Consumer problem */
+void put(int val)
+{
+    buffer[fill_ptr] = val; 
+    fill_ptr = (fill_ptr + 1) % MAX; 
+    count++; 
+    return; 
+}
+
+/* Helpers for Producer / Consumer problem */
+int get(void)
+{
+    int tmp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % MAX;
+    count--; 
+    return tmp; 
+}
+
+/* */
+void* producer(void* arg)
+{
+    int i;
+    for (i = 0; i < loops; i++)
+    {
+        /* LOCK HERE */
+        while (count == 0)
+        {
+            green_cond_wait(&cond);
+        }
+        put(i);
+        green_cond_signal(&cond); 
+        /* UNLOCK HERE*/
+    }
+    return (void*) 0; 
+}
+
+/**/
+void* consumer(void* arg)
+{
+    int i;
+    for (i = 0; i < loops; i++)
+    {
+        /* LOCK HERE */
+        while (count == 0)
+        {
+            green_cond_wait(&cond);
+        }
+        int tmp = get(); 
+        green_cond_signal(&cond); 
+
+        /* UNLOCK HERE*/
+        printf("%d\n", tmp);
+    }
+    return (void*) 0; 
+}
+
+
 int main(void)
 {
     green_t g0, g1; 
@@ -46,16 +159,24 @@ int main(void)
     green_cond_init(&cond);
 
     print_queue();
+    printf("Count: %d\n", count);
 
-    // green_create(&g0, test_thread, &a0);
-    // green_create(&g1, test_thread, &a1);
-    green_create(&g0, test_condv, &a0);
-    green_create(&g1, test_condv, &a1);
+    // green_create(&g0, test_yield, &a0);
+    // green_create(&g1, test_yield, &a1);
+    // green_create(&g0, test_condv, &a0);
+    // green_create(&g1, test_condv, &a1);
+    // green_create(&g0, test_interrupts, &a0);
+    // green_create(&g1, test_interrupts, &a1);
+    green_create(&g0, test_interrupts_fail, &a0);
+    green_create(&g1, test_interrupts_fail, &a1);
+
+    printf("Count: %d\n", count);
 
     print_queue();
 
     green_join(&g0, NULL);
     green_join(&g1, NULL); 
+    printf("Count: %d\n", count);
 
     print_queue();
     
